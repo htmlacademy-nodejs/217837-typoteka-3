da59be57ea422f1c62e9a9c97863ffe4b2f7e920
Britvin Aleksey <britvin.aleksej@yandex.ru>
2022-02-20 22:28:37 +0300

    Покоритель файлов

--- src/constants.js
+++ src/constants.js
@@ -9,4 +9,15 @@ const ExitCode = {
 
 const USER_ARGV_INDEX = 2;
 
-module.exports = {DEFAULT_COMMAND, ExitCode, USER_ARGV_INDEX};
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
+module.exports = {
+  DEFAULT_COMMAND,
+  ExitCode,
+  USER_ARGV_INDEX,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH
+};
