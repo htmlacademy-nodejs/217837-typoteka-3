8afab637f06cd9869721196b4f3269b3779f2345
Britvin Aleksey <krugombeda@gmail.com>
2022-07-02 20:34:27 +0400

    create errors templates

--- src/express/routes/general.js
+++ src/express/routes/general.js
@@ -7,6 +7,8 @@ generalRouter.get(`/`, (req, res) => res.render(`main`));
 generalRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 generalRouter.get(`/login`, (req, res) => res.render(`login`));
 generalRouter.get(`/search`, (req, res) => res.render(`search`));
+generalRouter.get(`/500`, (req, res) => res.render(`./errors/500`));
+generalRouter.get(`/404`, (req, res) => res.render(`./errors/404`));
 
 
 module.exports = generalRouter;
