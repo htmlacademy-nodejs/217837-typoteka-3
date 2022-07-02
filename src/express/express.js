45835ba9903f06bbba5052a907fbb8e7f95fd372
Britvin Aleksey <krugombeda@gmail.com>
2022-07-02 19:14:19 +0400

    set pug as view engine

--- src/express/express.js
+++ src/express/express.js
@@ -1,15 +1,21 @@
 "use strict";
 
 const express = require(`express`);
+const path = require(`path`);
 const articlesRouter = require(`./routes/articles`);
 const myRouter = require(`./routes/my`);
 const generalRouter = require(`./routes/general`);
 
 const PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 app.use(`/`, generalRouter);
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(PORT);
